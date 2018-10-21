f8ddacb0dc8ac4eede8b5ec8970125766faf8b66
Miko Planas <miksbon@gmail.com>
2018-10-21 11:25:38 +0800

    First
    No changes have been committed

---
+++ src/TTGameFiles/Game.java
@@ -0,0 +1,21 @@
+package TTGameFiles;
+import java.awt.Canvas;
+
+
+public class Game extends Canvas implements Runnable{
+
+
+    private static final long serialVersionUID = -4372403046120095479L;
+
+    public synchronized void start() {
+    }
+        public void run(){
+
+        }
+
+
+
+    public static void Main(String args[]){
+
+    }
+}
---
+++ src/TTGameFiles/Window.java
@@ -0,0 +1,29 @@
+package TTGameFiles;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+
+
+public class Window extends Canvas{
+
+    private static final long serialVersionUID = 1246506276288631690L;
+
+    public Window(int height, int width, String title, Game game){
+        JFrame frame = new JFrame(title);
+
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.add(game);
+        frame.setVisible(true);
+        game.start();
+
+    }
+
+}

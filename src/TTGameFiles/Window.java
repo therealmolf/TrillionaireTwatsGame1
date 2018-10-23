ede619bdd25b83ae8dc53497e0e289fe598ea7a8
Miko Planas <miksbon@gmail.com>
2018-10-23 08:59:26 +0800

    (src/TTGameFiles/Window.java)

--- src/TTGameFiles/Window.java
+++ src/TTGameFiles/Window.java
@@ -17,8 +17,11 @@ public class Window extends Canvas{
         frame.setMaximumSize(new Dimension(width, height));
         frame.setMinimumSize(new Dimension(width, height));
 
+// this is for the the Top right exit button to work
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+// this is so it cant be resizable
         frame.setResizable(false);
+//this just makes the frame open in the middle of the screen
         frame.setLocationRelativeTo(null);
         frame.add(game);
         frame.setVisible(true);
